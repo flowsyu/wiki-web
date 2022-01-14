764a66e8c2f1a245d83500e6f5b8e370e275f587
yudi1 <yudi1@xiaomi.com>
2022-01-14 17:22:36 +0800

    增加电子书管理页面 新页面开发：页面 路由 菜单

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,29 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import Home from '../views/home.vue'
+import About from '@/views/about'
+import AdminEbook from "@/views/admin/admin-ebook";
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/about.vue')
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: About
+    },
+    {
+        path: '/admin/ebook',
+        name: 'AdminEbook',
+        component: AdminEbook
+    },
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
---
+++ src/views/admin/admin-ebook.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">电子书管理</div>
+</template>
+
+<script>
+export default {
+  name: "admin-ebook"
+}
+</script>
+
+<style scoped>
+
+</style>
