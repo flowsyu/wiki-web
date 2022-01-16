45bd41ca79324e6d01b9b1f028c4ea8dcfb37482
yudi1 <yudi1@xiaomi.com>
2022-01-16 23:20:00 +0800

    集成message组件，请求异常时提示

--- src/views/admin/admin-ebook.vue
+++ src/views/admin/admin-ebook.vue
@@ -67,6 +67,7 @@
 <script>
 import {onMounted, ref} from "vue";
 import axios from "axios";
+import {message} from "ant-design-vue";
 
 export default {
   name: "AdminEbook",
@@ -74,7 +75,7 @@ export default {
     const ebooks = ref();
     const pagination = ref({
       current: 1,
-      pageSize: 4,
+      pageSize: 1001,
       total: 0,
     });
     const loading = ref(false);
@@ -129,11 +130,17 @@ export default {
       }).then((response) => {
         loading.value = false;
         const data = response.data;
-        ebooks.value = data.content.list;
 
-        // 重置分页按钮
-        pagination.value.current = params.page;
-        pagination.value.total = data.content.total;
+
+        if (data.success) {
+          ebooks.value = data.content.list;
+
+          // 重置分页按钮
+          pagination.value.current = params.page;
+          pagination.value.total = data.content.total;
+        } else {
+          message.error(data.message);
+        }
       });
     };
 
