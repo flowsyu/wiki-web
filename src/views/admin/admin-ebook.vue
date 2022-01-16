63e6a5653eedc2481d7c93a50219c398d6a21ce6
yudi1 <yudi1@xiaomi.com>
2022-01-16 22:57:47 +0800

    完成删除电子书功能

--- src/views/admin/admin-ebook.vue
+++ src/views/admin/admin-ebook.vue
@@ -23,9 +23,16 @@
           <a-button type="primary" @click="edit(record)">
             编辑
           </a-button>
-          <a-button type="danger">
-            删除
-          </a-button>
+          <a-popconfirm
+              title="删除后不可恢复，确认删除?"
+              ok-text="是"
+              cancel-text="否"
+              @confirm="handleDelete(record.id)"
+          >
+            <a-button type="danger">
+              删除
+            </a-button>
+          </a-popconfirm>
         </a-space>
       </template>
     </a-table>
@@ -51,7 +58,7 @@
         <a-input v-model:value="ebook.category2Id"/>
       </a-form-item>
       <a-form-item label="描述">
-        <a-input v-model:value="ebook.desc" type="textarea"/>
+        <a-input v-model:value="ebook.description" type="textarea"/>
       </a-form-item>
     </a-form>
   </a-modal>
@@ -181,6 +188,22 @@ export default {
       ebook.value = {};
     }
 
+    /**
+     * 删除
+     */
+    const handleDelete = (id) => {
+      axios.delete("/ebook/delete/" + id).then(response => {
+        const data = response.data;
+        if (data.success) {
+          //重新加载列表
+          handleQuery({
+            page: pagination.value.current,
+            size: pagination.value.pageSize,
+          });
+        }
+      });
+    }
+
 
     onMounted(() => {
       handleQuery({
@@ -200,7 +223,8 @@ export default {
       handleModalOk,
       edit,
       ebook,
-      add
+      add,
+      handleDelete
     }
   }
 }
