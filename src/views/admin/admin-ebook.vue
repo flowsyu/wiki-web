b85f199a671d94069bdb9d5502783af36ed31f08
yudi1 <yudi1@xiaomi.com>
2022-01-16 14:56:26 +0800

    完成电子书编辑功能，编辑成功后刷新列表

--- src/views/admin/admin-ebook.vue
+++ src/views/admin/admin-ebook.vue
@@ -139,15 +139,24 @@ export default {
     /**
      * 表单
      */
-    const ebook = ref({});
-    const modalVisible = ref(false);
-    const modalLoading = ref(false);
+    let ebook = ref({});
+    let modalVisible = ref(false);
+    let modalLoading = ref(false);
     const handleModalOk = () => {
       modalLoading.value = true;
-      setTimeout(() => {
-        modalVisible.value = false;
-        modalLoading.value = false;
-      }, 2000);
+      axios.post("/ebook/save", ebook.value).then(response => {
+        const data = response.data;// data = commonResp
+        if (data.success) {
+          modalVisible.value = false;
+          modalLoading.value = false;
+
+          // 重新加载列表
+          handleQuery({
+            page: pagination.value.current,
+            size: pagination.value.pageSize,
+          });
+        }
+      });
     };
 
     /**
