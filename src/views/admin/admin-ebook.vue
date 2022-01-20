ad908f01756cb9e036f6fac9507545e34fc973ca
yudi1 <yudi1@xiaomi.com>
2022-01-20 15:26:11 +0800

    增加按名字查询电子书功能

--- src/views/admin/admin-ebook.vue
+++ src/views/admin/admin-ebook.vue
@@ -3,9 +3,22 @@
       :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
   >
     <p>
-      <a-button type="primary" @click="add" size="large">
-        新增
-      </a-button>
+      <a-form layout="inline" :model="param">
+        <a-form-item>
+          <a-input v-model:value="param.name" placeholder="名称">
+          </a-input>
+        </a-form-item>
+        <a-form-item>
+          <a-button type="primary" @click="handleQuery({page: 1, size: pagination.pageSize})">
+            查询
+          </a-button>
+        </a-form-item>
+        <a-form-item>
+          <a-button type="primary" @click="add()">
+            新增
+          </a-button>
+        </a-form-item>
+      </a-form>
     </p>
     <a-table
         :columns="columns"
@@ -72,6 +85,9 @@ import {message} from "ant-design-vue";
 export default {
   name: "AdminEbook",
   setup() {
+    const param = ref();
+    param.value = {};
+
     const ebooks = ref();
     const pagination = ref({
       current: 1,
@@ -125,7 +141,8 @@ export default {
       axios.get("/ebook/list", {
         params: {
           page: params.page,
-          size: params.size
+          size: params.size,
+          name: param.value.name
         }
       }).then((response) => {
         loading.value = false;
@@ -232,7 +249,9 @@ export default {
       edit,
       ebook,
       add,
-      handleDelete
+      handleDelete,
+      param,
+      handleQuery
     }
   }
 }
